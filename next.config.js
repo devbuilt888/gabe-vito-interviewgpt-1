7f45ad29f35585344ca36ba23011aba9a44abc7d
zero <moneymavie@gmail.com>
2025-03-28 19:21:56 -0400

    new netlify config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
+  webpack: (config, { isServer }) => {
+    // Add a rule to handle canvas.node
+    config.module.rules.push({
+      test: /\.node$/,
+      use: 'node-loader',
+    });
+
+    // Add a rule to handle canvas
+    config.module.rules.push({
+      test: /canvas/,
+      use: 'null-loader',
+    });
+
+    // Add a rule to handle three.js
+    config.module.rules.push({
+      test: /three/,
+      use: 'null-loader',
+    });
+
+    return config;
   },
+  // Disable image optimization for Netlify
+  images: {
+    unoptimized: true,
+  },
+  // Add transpilePackages for three.js
+  transpilePackages: ['three'],
 };
 
 module.exports = nextConfig
